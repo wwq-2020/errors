fa62a5779fd905768ee1466e1262c21971b26b7a
wwq <2992395632@qq.com>
2020-01-03 11:28:39 +0800

    feat: compatible go1.13 error

--- errors.go
+++ errors.go
@@ -1,9 +1,8 @@
 package errors
 
 import (
+	"errors"
 	"fmt"
-	"strings"
-	"sync"
 
 	"github.com/wwq1988/errors/stack"
 )
@@ -18,93 +17,72 @@ type temporary interface {
 	Temporary() bool
 }
 
-type stackError struct {
-	fields stack.Fields
-	once   sync.Once
-	stacks []string
+// StackError StackError
+type StackError struct {
+	fields map[string]stack.Fields
 	err    error
 }
 
-// New 初始化stackError
+// New 初始化StackError
 func New(msg string, args ...interface{}) error {
 	return NewEx(2, fmt.Errorf(msg, args...), nil)
 }
 
-// NewWithFields 初始化stackError带域存储
+// NewWithFields 初始化StackError带域存储
 func NewWithFields(msg string, fields stack.Fields) error {
 	return NewEx(2, fmt.Errorf(msg), fields)
 }
 
-// NewWithField 初始化stackError带kv
+// NewWithField 初始化StackError带kv
 func NewWithField(msg string, key string, val interface{}) error {
 	fs := stack.New().Set(key, val)
 	return NewEx(2, fmt.Errorf(msg), fs)
 }
 
-// NewEx 初始化stackError,带堆栈深度和域存储
+// NewEx 初始化StackError,带堆栈深度和域存储
 func NewEx(depth int, err error, fields stack.Fields) error {
 	stackFrame := stack.Get(depth)
-	se, ok := err.(*stackError)
+	if fields == nil {
+		fields = stack.New()
+	}
+	se, ok := err.(*StackError)
 	if !ok {
-		if fields == nil {
-			fields = stack.New()
-		}
-		return &stackError{
+		se = &StackError{
 			err:    err,
-			fields: fields,
-			stacks: []string{stackFrame},
+			fields: make(map[string]stack.Fields),
 		}
 	}
-	se.stacks = append(se.stacks, stackFrame)
-	if se.fields == nil {
-		se.fields = stack.New()
-	}
-	if fields != nil {
-		se.fields.Merge(fields)
-	}
+	se.fields[stackFrame] = fields
 	return se
 }
 
-// WithField 携带域存储
-func (s *stackError) WithField(key string, val interface{}) {
-	s.fields.Set(key, val)
-}
-
-// WithField 携带域存储
-func (s *stackError) WithFields(fields stack.Fields) {
-	s.fields.Merge(fields)
-}
-
 // Fields 获取域存储
-func (s *stackError) Fields() stack.Fields {
-	s.once.Do(s.fillStackField)
+func (s *StackError) Fields() map[string]stack.Fields {
 	return s.fields
 }
 
-func (s *stackError) fillStackField() {
-	s.fields.Set("stack", s.stack())
-}
-
-func (s *stackError) stack() string {
-	if len(s.stacks) == 0 {
-		return ""
-	}
-	return strings.Join(s.stacks, ";")
-}
-
-func (s *stackError) cleanFields() {
-	s.fields = stack.New()
-}
-
-func (s *stackError) Unwrap() error {
+// Unwrap Unwrap
+func (s *StackError) Unwrap() error {
 	return s.err
 }
 
-func (s *stackError) Is(err error) bool {
-	return s.Unwrap() == Unwrap(err)
+// Is Is
+func (s *StackError) Is(err error) bool {
+	se, ok := err.(*StackError)
+	return ok && se.err == s.err
 }
 
-func (s *stackError) Error() string {
+// As As
+func (s *StackError) As(err error) bool {
+	se, ok := err.(*StackError)
+	if ok {
+		*se = *s
+	}
+	return ok
+}
+
+// Error Error
+func (s *StackError) Error() string {
 	return s.err.Error()
 }
 
@@ -144,15 +122,12 @@ func TraceWithField(err error, key string, val interface{}) error {
 
 // Is 判断err是否相同
 func Is(src, dst error) bool {
-	return Unwrap(src) == Unwrap(dst)
+	return errors.Is(src, dst)
 }
 
 // Unwrap 返回原始err
 func Unwrap(err error) error {
-	if stackErr, ok := err.(*stackError); ok {
-		return stackErr.Unwrap()
-	}
-	return err
+	return errors.Unwrap(err)
 }
 
 // IsTimeout 判断是否超时
@@ -168,9 +143,9 @@ func IsTemporary(err error) bool {
 }
 
 // Fields 获取域
-func Fields(err error) stack.Fields {
-	if stackErr, ok := err.(*stackError); ok {
+func Fields(err error) map[string]stack.Fields {
+	if stackErr, ok := err.(*StackError); ok {
 		return stackErr.Fields()
 	}
-	return stack.New()
+	return nil
 }
